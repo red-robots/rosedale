31408f7f0b50660cc74becafc257da2c89400b65
fritzhealy <fre.healy@gmail.com>
2018-05-08 14:31:11 -0400

    adding fixed sidebar

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -85,7 +85,78 @@ jQuery(document).ready(function ($) {
 		$("a[href^=#]").removeClass("active");
 		$(e.target).addClass("active");
 		var href = e.target.hash.substring(1);
-		$('html, body').animate({scrollTop: $("a[name="+href+"]").offset().top}, 500);
+		$('html, body').animate({scrollTop: $("a[name="+href+"]").offset().top}, 500,function(){
+			if($col.length>0){
+				sticky_sidebar();
+			}
+		});
 	});
+
+	var $col = $('.template-history >.wrapper >.wrapper >.row-2 >.col-2');
+	if($col.length>0){
+		var $window = $(window);
+		function sticky_sidebar(){
+			var window_top = $window.scrollTop();
+			var window_width = window.innerWidth;
+			var $wrapper = $('.template-history >.wrapper >.wrapper');
+			var anchor_top = $('.template-history >.wrapper >.wrapper >.row-2').offset().top;
+			var anchor_bottom = $('.template-history >.wrapper >.wrapper >.row-2').offset().top +  $('.template-history >.wrapper >.wrapper >.row-2').outerHeight();
+			if(window_width>=600&&window_top>=anchor_top){
+				if($col.outerHeight()+window_top>=anchor_bottom){
+					if(window_width>=900){
+						$col.css({
+							position: 'fixed',
+							top: -1*($col.outerHeight()-(anchor_bottom-window_top)),
+							backgroundColor: '#E9F5F1',
+							maxWidth: '364px',
+							right:parseFloat($wrapper.css("paddingRight"))+(parseFloat(window.innerWidth) - parseFloat($wrapper.offset().left) - parseFloat($wrapper.outerWidth())),
+							width: '28%'
+						});
+					} else {
+						$col.css({
+							position: 'fixed',
+							top: -1*($col.outerHeight()-(anchor_bottom-window_top)),
+							backgroundColor: '#E9F5F1',
+							maxWidth: '364px',
+							right:parseFloat($wrapper.css("paddingRight"))+(parseFloat(window.innerWidth) - parseFloat($wrapper.offset().left) - parseFloat($wrapper.outerWidth())),
+							width: '34%'
+						});
+					}
+				} else {
+					if(window_width>=900){
+						$col.css({
+							position: 'fixed',
+							top: 0,
+							maxWidth: '364px',
+							backgroundColor: '#E9F5F1',
+							right:parseFloat($wrapper.css("paddingRight"))+(parseFloat(window.innerWidth) - parseFloat($wrapper.offset().left) - parseFloat($wrapper.outerWidth())),
+							width: '28%'
+						});
+					} else {
+						$col.css({
+							position: 'fixed',
+							top: 0,
+							maxWidth: '364px',
+							backgroundColor: '#E9F5F1',
+							right:parseFloat($wrapper.css("paddingRight"))+(parseFloat(window.innerWidth) - parseFloat($wrapper.offset().left) - parseFloat($wrapper.outerWidth())),
+							width: '34%'
+						});
+					}
+				}
+			} else {
+				$col.css({
+					position:'',
+					top: '',
+					backgroundColor: '',
+					right: '',
+					width: '',
+					maxWidth: ''
+				});
+			}
+		}
+		sticky_sidebar();
+		$window.on('scroll',sticky_sidebar);
+		$window.on('resize',sticky_sidebar);
+	}
 
 });// END #####################################    END
--- template-parts/content-history.php
+++ template-parts/content-history.php
@@ -16,54 +16,56 @@
             <img src="<?php echo $banner['url'];?>" alt="<?php echo $banner['alt'];?>">
         </div><!--.banner-->
     <?php endif;?>
-    <div class="wrapper cap internal">
-        <header class="row-1">
-            <h1><?php the_title();?></h1>
-        </header>
-        <?php $sections = get_field("sections");?>
-        <div class="row-2 clear-bottom">
-            <aside class="col-2">
-                <?php if($sections):?>
-                    <ul>
-                        <?php foreach($sections as $section):
-                            $title = $section['title'];
-                            if($title):?>
-                                <li>
-                                    <a href="#<?php echo strtolower(preg_replace('/[^0-9A-Za-z]/','',sanitize_title_with_dashes($title)));?>">
-                                        <?php echo $title;?>
-                                    </a>
-                                </li>
-                            <?php endif;
-                        endforeach;?>
-                    </ul>
-                <?php endif;?>
-            </aside><!--.col-2-->
-            <div class="col-1">
-                <?php if($sections):?>
-                    <div class="sections">
-                        <?php foreach($sections as $section):
-                            $title = $section['title'];
-                            $copy = $section['copy'];
-                            if($title||$copy):?>
-                                <section class="section">
-                                    <?php if($title):?>
-                                        <a name="<?php echo strtolower(preg_replace('/[^0-9A-Za-z]/','',sanitize_title_with_dashes($title)));?>"></a>
-                                        <header>
-                                            <h2><?php echo $title;?></h2>
-                                        </header>
-                                    <?php endif;
-                                    if($copy):?>
-                                        <div class="copy">
-                                            <?php echo $copy;?>
-                                        </div><!--.copy-->
-                                    <?php endif;?>
-                                </section><!--.section-->
-                            <?php endif;
-                        endforeach;?>
-                    </div><!--.sections-->
-                <?php endif;?>
-            </div><!--.col-1-->
-        </div><!--.row-3-->
-        <?php get_template_part( 'template-parts/upcoming' );?>
+    <div class="wrapper cap-nopad">
+        <div class="wrapper cap internal">
+            <header class="row-1">
+                <h1><?php the_title();?></h1>
+            </header>
+            <?php $sections = get_field("sections");?>
+            <div class="row-2 clear-bottom">
+                <aside class="col-2">
+                    <?php if($sections):?>
+                        <ul>
+                            <?php foreach($sections as $section):
+                                $title = $section['title'];
+                                if($title):?>
+                                    <li>
+                                        <a href="#<?php echo strtolower(preg_replace('/[^0-9A-Za-z]/','',sanitize_title_with_dashes($title)));?>">
+                                            <?php echo $title;?>
+                                        </a>
+                                    </li>
+                                <?php endif;
+                            endforeach;?>
+                        </ul>
+                    <?php endif;?>
+                </aside><!--.col-2-->
+                <div class="col-1">
+                    <?php if($sections):?>
+                        <div class="sections">
+                            <?php foreach($sections as $section):
+                                $title = $section['title'];
+                                $copy = $section['copy'];
+                                if($title||$copy):?>
+                                    <section class="section">
+                                        <?php if($title):?>
+                                            <a name="<?php echo strtolower(preg_replace('/[^0-9A-Za-z]/','',sanitize_title_with_dashes($title)));?>"></a>
+                                            <header>
+                                                <h2><?php echo $title;?></h2>
+                                            </header>
+                                        <?php endif;
+                                        if($copy):?>
+                                            <div class="copy">
+                                                <?php echo $copy;?>
+                                            </div><!--.copy-->
+                                        <?php endif;?>
+                                    </section><!--.section-->
+                                <?php endif;
+                            endforeach;?>
+                        </div><!--.sections-->
+                    <?php endif;?>
+                </div><!--.col-1-->
+            </div><!--.row-3-->
+            <?php get_template_part( 'template-parts/upcoming' );?>
+        </div><!--.wrapper-->
     </div><!--.wrapper-->
 </article><!-- #post-## -->
